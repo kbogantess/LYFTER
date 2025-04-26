d2e53efd804c4a6b1967da8186f2c67f90a54d12
kevin Bogantes <bogantes87@gmail.com>
2025-04-26 02:43:54 -0600

    Añadiendo semana 5 del frontend

---
+++ Semana_5(M2)/Semana_5(M2)_frontend/METODS EXAMPLE/forEach2.js
@@ -0,0 +1,15 @@
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((item, index, arr) => {
+    console.log( "index:" + index + " item:" + item );
+});
+
+
+'index:0 item:1'
+'index:1 item:2'
+'index:2 item:3'
+'index:3 item:4'
+'index:4 item:5'
+
